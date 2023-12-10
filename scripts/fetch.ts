ad7924d0318e21f3abd6a1df19612e9093b52ace
danelliottpalmer <danelliottpalmer@gmail.com>
2023-12-10 17:59:19 -0500

    add gamesradarfetcher

--- scripts/fetch.ts
+++ scripts/fetch.ts
@@ -1,17 +1,19 @@
 import { fetcher as wikipediaFetcher } from "../src/fetchers/WikipediaFetcher.ts";
 import { fetcher as gameInformerFetcher } from "../src/fetchers/GameInformerFetcher.ts";
+import { fetcher as gamesRadarFetcher } from "../src/fetchers/GamesRadarFetcher.tsx";
 import { VIDEO_GAME_MANAGER } from "../src/game/index.ts";
 
 const fetchers = [
   wikipediaFetcher,
   gameInformerFetcher,
+  gamesRadarFetcher,
 ];
 
 async function main() {
   for (const fetcher of fetchers) {
     await fetcher.fetch();
     const games = fetcher.extract();
-    VIDEO_GAME_MANAGER.add(games, true);
+    VIDEO_GAME_MANAGER.add(games, fetcher !== wikipediaFetcher);
     console.log("Total games", VIDEO_GAME_MANAGER.length);
   }
   console.log(Array.from(VIDEO_GAME_MANAGER));
